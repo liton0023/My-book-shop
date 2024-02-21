0de86cc413089e34e3d3c97d29a288aca47c8ba0
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-22 01:10:58 +0600

    create comment section ui

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -4,7 +4,9 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
-import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { AuthContext } from "../../../Provider/AuthProvider/AuthProvider";
 import useCart from "../Hooks/useCart";
@@ -50,12 +52,17 @@ const BookDetails = () => {
         fetchBook();
     },[params.id]);
 
+    // console.log(bookDetails.imgUrl)
+    // bookDetails.imgUrl.map((url)=>{
+    //   console.log(url)
+    // })
+
 
     const handleAddToCart =(item)=>{
-        const {_id,name ,Book,category,Instoke,description,discountPrice,offer,regularPrice,stock,writer}=item;
+        const {_id,name ,Book,category,Instoke,description,discountPrice,offer,regularPrice,stock,writer,imgUrl}=item;
         console.log(item)
         if(user && user.email){
-          const orderItem ={menuItemId : _id,name:name,Book,category,Instoke,description,discountPrice,offer,regularPrice,stock,writer, email: user.email}
+          const orderItem ={menuItemId : _id,name:name,Book,category,Instoke,description,discountPrice,offer,regularPrice,stock,writer, email: user.email,imgUrl}
           fetch('http://localhost:5000/carts',{
             method: 'POST',
             headers:{
@@ -105,8 +112,19 @@ const BookDetails = () => {
         )}
         {bookDetails && !loading && !error && (
           <div>
-            <Swiper navigation>
-              {bookDetails.imgUrl.map((url) => (
+            <Swiper  spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}>
+              {bookDetails?.imgUrl.length >0 &&
+               bookDetails?.imgUrl.map((url) => (
                 <SwiperSlide key={url}>
                   <div
                     className='h-[550px]'
@@ -191,6 +209,33 @@ const BookDetails = () => {
               ADD TO CART
             </button>
           </div>
+       <form action="">
+       <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+     <div>
+     <button
+              // onClick={() => handleAddToCart(bookDetails)}
+              className="btn btn-outline uppercase border-0 border-b-4 mt-4"
+            >
+              Comment
+            </button>
+     </div>
+       </form>
+          <div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Obi-Wan Kenobi
+    <time className="text-xs opacity-50">12:45</time>
+  </div>
+  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-footer opacity-50">
+    Delivered
+  </div>
+</div>
+
             </div>
           </div>
         )}
